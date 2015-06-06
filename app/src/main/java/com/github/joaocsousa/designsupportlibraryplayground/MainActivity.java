2cf326ee8d3b581ba207f56446ed55b9923edfbe
Joao Sousa <jcruzsou@gmail.com>
2015-06-06 12:24:58 +0100

    Added FAB.

--- app/src/main/java/com/github/joaocsousa/designsupportlibraryplayground/MainActivity.java
+++ app/src/main/java/com/github/joaocsousa/designsupportlibraryplayground/MainActivity.java
@@ -1,24 +1,35 @@
 package com.github.joaocsousa.designsupportlibraryplayground;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
 	private boolean mNameInputErrorEnabled = false;
 
+	private FloatingActionButton mAddButton;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		mAddButton = (FloatingActionButton) findViewById(R.id.activity_main_add_button);
 		findViewById(R.id.activity_main_toggle_name_error_button).setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				toggleNameInputError();
+			}
+		});
+		mAddButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(MainActivity.this, "Action Add", Toast.LENGTH_SHORT).show();
 			}
 		});
 	}
