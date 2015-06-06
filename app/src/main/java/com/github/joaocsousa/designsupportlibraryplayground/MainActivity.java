f6df5d39b4010f7876b6433464c3212f638ffe7a
Joao Sousa <jcruzsou@gmail.com>
2015-06-06 11:38:40 +0100

    Fixed compilation. Added working example for TextInputLayout.

--- app/src/main/java/com/github/joaocsousa/designsupportlibraryplayground/MainActivity.java
+++ app/src/main/java/com/github/joaocsousa/designsupportlibraryplayground/MainActivity.java
@@ -1,16 +1,36 @@
 package com.github.joaocsousa.designsupportlibraryplayground;
 
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+	private boolean mNameInputErrorEnabled = false;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		findViewById(R.id.activity_main_toggle_name_error_button).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				toggleNameInputError();
+			}
+		});
+	}
+
+	private void toggleNameInputError() {
+		mNameInputErrorEnabled = !mNameInputErrorEnabled;
+		updateNameInputErrorState();
+	}
+
+	private void updateNameInputErrorState() {
+		TextInputLayout nameInputErrorContainer = ((TextInputLayout) findViewById(R.id.activity_main_input_name_container));
+		nameInputErrorContainer.setError(mNameInputErrorEnabled ? "This is an error message." : "");
 	}
 
 }
